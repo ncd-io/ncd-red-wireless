ea744d503288a26a77c9c9cd1f64eda244b5f39e
Trey Felton <trey@controleverything.com>
2018-04-27 11:29:22 -0500

    Version 2 initial commit

---
+++ test.js
@@ -0,0 +1,51 @@
+const NCD = require('./index.js');
+const comm = require('ncd-red-comm');
+
+var serial = new comm.NcdSerial('/dev/tty.usbserial-A106EM3X', 9600);
+var modem = new NCD.Modem(serial);
+var gateway = new NCD.Gateway(modem);
+
+gateway.on('found_sensor', (sensor) => {
+	console.log('sensor of type '+sensor.type+' found at '+sensor.mac);
+});
+gateway._emitter.once('sensor_mode', (sensor) => {
+	if(sensor.mode == "PGM"){
+		console.log('Getting Pan ID');
+		//modem.send.remote_at_command(mac2bytes(sensor.mac), 'ID').then(console.log).catch(console.log);
+		var broadcast = "00:00:00:00:00:00:FF:FF"
+		setTimeout(() => {
+			 gateway.config_get_pan_id(sensor.mac).then(console.log).catch(console.log);
+			// gateway.config_set_pan_id(sensor.mac, 0x7BCE).then(console.log).catch(console.log);
+		}, 500);
+	}
+});
+gateway.on('sensor_mode', (sensor) => {
+	console.log('sensor at '+sensor.mac+' now in '+sensor.mode+' mode');
+});
+gateway.on('sensor_data', (data) => {
+	console.log(data);
+});
+
+
+
+modem.send.at_command("ID").then(() => {
+	//gateway.config_set_pan_id("00:13:a2:00:41:57:16:e1", 0x7BCE).then(console.log).catch(console.log);
+	 gateway.config_get_pan_id("00:13:a2:00:41:57:16:e1").then(console.log).catch(console.log);
+}).catch((e) => {
+	console.log({error: e});
+});
+
+
+// var sensor = new NCD.Sensor(modem);
+//
+// sensor.on('power_up', console.log);
+
+// gateway.config_get_pan_id('00:13:a2:00:41:07:18:81').then((res) => {
+// 	console.log('pan id:');
+// 	console.log(res);
+// });
+// 7e 00 06 09 5b 49 44 7b cd c6
+// 7e 00 04 09 5b 49 44 0e
+function mac2bytes(mac){
+	return mac.split(':').map((v) => parseInt(v, 16));
+}
--- wireless.js
+++ wireless.js
@@ -1,22 +1,209 @@
-const ncd = require("./index.js");
+const wireless = require("./index.js");
+const comms = require('ncd-red-comm');
+const sp = require('serialport');
+const Queue = require("promise-queue");
+
 module.exports = function(RED) {
-	function NcdWirelessNode(config) {
-		RED.nodes.createNode(this, config);
+	var gateway_pool = {};
+
+	function NcdGatewayConfig(config){
+		RED.nodes.createNode(this,config);
+        this.port = config.port;
+		this.baudRate = parseInt(config.baudRate);
+		this.sensor_pool = [];
+		if(typeof gateway_pool[this.bus] != 'undefined'){
+			if(this.baudRate != gateway_pool[this.bus].digi.serial.baudRate){
+				gateway_pool[this.bus].digi.serial.update({baudRate: this.baudRate}).then().catch(console.log);
+			}
+		}else{
+			var serial = new comms.NcdSerial(this.port, this.baudRate);
+			var modem = new wireless.Modem(serial);
+			gateway_pool[this.bus] = new wireless.Gateway(modem)
+		}
+		this.gateway = gateway_pool[this.bus];
+	}
+	RED.nodes.registerType("ncd-gateway-config", NcdGatewayConfig);
+
+	function NcdGatewayNode(config){
+		RED.nodes.createNode(this,config);
+		this.gateway = RED.nodes.getNode(config.connection).gateway;
+
 		var node = this;
-		node.modem = new ncd.Wireless(ncd.Serial(config.port, config.baudRate));
-		node.modem.on('sensor-packet', (packet) => {
-			packet.payload = packet.readings;
-			node.send(packet);
+		node.is_config = false;
+		node.check_mode = function(cb){
+			node.gateway.digi.send.at_command("ID").then((res) => {
+				var pan_id = (res.data[0] << 8) | res.data[1];
+				if(pan_id == 0x7BCD){
+					node.is_config = true;
+					node.status({fill:"yellow",shape:"ring",text:"Configuring"});
+				}else{
+					node.status({fill:"green",shape:"dot",text:"Ready"});
+					node.is_config = false;
+				}
+				if(cb) cb(node.is_config ? "in configuration mode" : "in listening mode");
+			}).catch(() => {
+				node.status({fill:"red",shape:"dot",text:"Failed to Connect"});
+				if(cb) cb("unable to connect");
+			});
+		}
+		node.gateway.on('sensor_data', (d) => node.send({topic: 'sensor_data', payload: d}));
+		node.on('close', () => {
+			node.gateway._emitter.removeAllListeners('sensor_data');
 		});
-		node.on('close', (removed, done) => {
-			node.modem.close();
-			delete(node.modem);
-			done();
+		node.check_mode();
+	}
+	RED.nodes.registerType("ncd-gateway-node", NcdGatewayNode);
+
+
+	function NcdWirelessNode(config){
+		RED.nodes.createNode(this,config);
+		this.gateway = RED.nodes.getNode(config.connection).gateway;
+		var dedicated_config = false;
+		this.config_gateway = this.gateway;
+		if(config.config_gateway){
+			this.config_gateway = RED.nodes.getNode(config.config_gateway).gateway;
+			dedicated_config = true;
+		}
+		this.queue = new Queue(1);
+		var node = this;
+		var modes = {
+			PGM: {fill:"red",shape:"dot",text:"Config Mode"},
+			RUN: {fill:"green",shape:"dot",text:"Running"},
+			READY: {fill: "green", shape: "ring", text:"Config Complete"}
+		}
+		var events = {};
+		var pgm_events = {};
+		this.gtw_on = (event, cb) => {
+			events[event] = cb;
+			this.gateway.on(event, cb);
+		}
+		this.pgm_on = (event, cb) => {
+			events[event] = cb;
+			this.config_gateway.on(event, cb);
+		}
+
+		function _config(mac){
+			node.queue.add(() => {
+				return new Promise((fulfill, reject) => {
+					setTimeout(fulfill, 500);
+				});
+			});
+			// node.queue.add(() => {
+			// 	return node.gateway.config_set_destination(mac, config.destination);
+			// });
+			node.queue.add(() => {
+				return node.gateway.config_set_id_delay(mac, parseInt(config.node_id), parseInt(config.delay));
+			});
+			// node.queue.add(() => {
+			// 	return node.gateway.config_set_power(mac, config.power);
+			// });
+			// node.queue.add(() => {
+			// 	return node.gateway.config_set_retries(mac, config.retries);
+			// });
+			// node.queue.add(() => {
+			// 	return node.gateway.config_set_pan_id(mac, config.pan_id);
+			// });
+			node.queue.add(() => {
+				return new Promise((fulfill, reject) => {
+					node.status(modes.READY);
+					fulfill();
+				});
+			});
+		}
+		if(config.addr){
+			RED.nodes.getNode(config.connection).sensor_pool.push(config.addr);
+			this.gtw_on('sensor_data-'+config.addr, (data) => {
+				node.send({
+					topic: 'sensor_data',
+					data: data,
+					payload: data.sensor_data
+				});
+			});
+			this.pgm_on('sensor_mode-'+config.addr, (sensor) => {
+				node.status(modes[sensor.mode]);
+				if(config.auto_config && sensor.mode == "PGM") _config(sensor.mac);
+			});
+		}else if(config.sensor_type){
+			this.gtw_on('sensor_data-'+config.sensor_type, (data) => {
+				node.send({
+					topic: 'sensor_data',
+					data: data,
+					payload: data.sensor_data
+				});
+			});
+
+			// this.gateway.on('sensor_mode', (sensor) => {
+			// 	if(sensor.sensor_type == config.sensor_type){
+			// 		node.status(modes[sensor.mode]);
+			// 	}
+			// });
+			this.pgm_on('sensor_mode', (sensor) => {
+				if(sensor.sensor_type == config.sensor_type){
+					node.status(modes[sensor.mode]);
+					if(config.auto_config) _config(sensor.mac);
+				}
+			});
+		}
+		this.on('close', () => {
+			for(var e in events){
+				node.gateway._emitter.removeAllListeners(e);
+			}
+			for(var p in pgm_events){
+				node.config_gateway._emitter.removeAllListeners(p);
+			}
 		});
 	}
-	RED.nodes.registerType("Ncd-Wireless", NcdWirelessNode);
+	RED.nodes.registerType("ncd-wireless-node", NcdWirelessNode);
+
+	RED.httpAdmin.post("/ncd/wireless/gateway/config/:id", RED.auth.needsPermission("serial.read"), function(req,res) {
+        var node = RED.nodes.getNode(req.params.id);
+        if (node != null) {
+            try {
+				var pan = node.is_config ? [0x7f, 0xff] : [0x7b, 0xcd];
+				node.gateway.digi.send.at_command("ID", pan).then().catch().then(() => {
+					node.check_mode((m) => res.send(m));
+				});
+            } catch(err) {
+                res.sendStatus(500);
+                node.error(RED._("inject.failed",{error:err.toString()}));
+            }
+        } else {
+            res.sendStatus(404);
+        }
+    });
+
 	RED.httpAdmin.get("/ncd/wireless/modems/list", RED.auth.needsPermission('serial.read'), function(req,res) {
-		ncd.SerialList(res);
+		getSerialDevices(true, res);
+	});
+	RED.httpAdmin.get("/ncd/wireless/sensors/list/:id", RED.auth.needsPermission('serial.read'), function(req,res) {
+		var node = RED.nodes.getNode(req.params.id);
+        if (node != null) {
+            try {
+				var sensors = [];
+				for(var i in node.gateway.sensor_pool){
+					if(node.sensor_pool.indexOf(node.gateway.sensor_pool[i].mac) > -1) continue;
+					sensors.push(node.gateway.sensor_pool[i])
+				}
+				res.json(sensors);
+            } catch(err) {
+                res.sendStatus(500);
+                node.error(RED._("inject.failed",{error:err.toString()}));
+            }
+        } else {
+            res.sendStatus(404);
+        }
 	});
 
+}
+function getSerialDevices(ftdi, res){
+	var busses = [];
+	sp.list().then((ports) => {
+		ports.forEach((p) => {
+			busses.push(p.comName);
+		});
+	}).catch((err) => {
+
+	}).then(() => {
+		res.json(busses);
+	});
 }
