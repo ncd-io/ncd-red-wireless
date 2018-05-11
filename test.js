8f15e9c971cfb1e76eabd4329c1d4e46b8b0e23f
Trey Felton <trey@controleverything.com>
2018-05-11 15:30:31 -0500

    Updated test

--- test.js
+++ test.js
@@ -5,48 +5,12 @@ var serial = new comm.NcdSerial('/dev/tty.usbserial-A106EM3X', 9600);
 var modem = new NCD.Modem(serial);
 var gateway = new NCD.Gateway(modem);
 
-// gateway.on('found_sensor', (sensor) => {
-// 	console.log('sensor of type '+sensor.type+' found at '+sensor.mac);
-// });
-// gateway._emitter.once('sensor_mode', (sensor) => {
-// 	if(sensor.mode == "PGM"){
-// 		console.log('Getting Pan ID');
-// 		//modem.send.remote_at_command(mac2bytes(sensor.mac), 'ID').then(console.log).catch(console.log);
-// 		var broadcast = "00:00:00:00:00:00:FF:FF"
-// 		setTimeout(() => {
-// 			//  gateway.config_get_pan_id(sensor.mac).then(console.log).catch(console.log);
-// 			 //gateway.config_set_pan_id(sensor.mac, 0x7FFF).then(console.log).catch(console.log);
-// 		}, 500);
-// 	}
-// });
-// gateway.on('sensor_mode', (sensor) => {
-// 	console.log('sensor at '+sensor.mac+' now in '+sensor.mode+' mode');
-// });
-// gateway.on('sensor_data', (data) => {
-// 	console.log(data);
-// });
-
-
-console.log('getting pan id?');
-modem.send.at_command("ID").then((res) => {
-	console.log(res);
-	//gateway.config_set_pan_id("00:13:a2:00:41:57:16:e1", 0x7BCE).then(console.log).catch(console.log);
-	 //gateway.config_get_pan_id("00:13:a2:00:41:57:16:e1").then(console.log).catch(console.log);
-}).catch((e) => {
-	console.log({error: e});
+gateway.on('sensor_data', (d) => {
+	var type = gateway.sensor_types[d.sensor_type].name;
+	console.log("Incoming data -------------------");
+	console.log("Type: "+type);
+	console.log("Address: "+d.addr);
+	console.log("Readings: ");
+	for(var i in d.sensor_data) console.log(`	${i}: ${d.sensor_data[i]}`);
+	console.log("---------------------------------")
 });
-
-
-// var sensor = new NCD.Sensor(modem);
-//
-// sensor.on('power_up', console.log);
-
-// gateway.config_get_pan_id('00:13:a2:00:41:07:18:81').then((res) => {
-// 	console.log('pan id:');
-// 	console.log(res);
-// });
-// 7e 00 06 09 5b 49 44 7b cd c6
-// 7e 00 04 09 5b 49 44 0e
-function mac2bytes(mac){
-	return mac.split(':').map((v) => parseInt(v, 16));
-}
