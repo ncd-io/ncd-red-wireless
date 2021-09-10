3398ecd7029cb5d05adc0b88df9fdecb1d3e89f6
Jacob Jacobson <jacob@ncd.io>
2021-09-10 14:43:15 -0500

    Updated 101 config to not update enabled axis if no axis selected

--- wireless.js
+++ wireless.js
@@ -331,8 +331,9 @@ module.exports = function(RED) {
 								if(config.sampling_duration_101_active){
 									promises.sampling_duration_101 = node.config_gateway.config_set_sampling_duration_101(mac, parseInt(config.sampling_duration_101));
 								}
-
-								promises.axis_enabled_101 = node.config_gateway.config_set_axis_enabled_101(mac, config.x_axis_101, config.y_axis_101, config.z_axis_101);
+								if(config.x_axis_101 || config.y_axis_101 || config.z_axis_101){
+									promises.axis_enabled_101 = node.config_gateway.config_set_axis_enabled_101(mac, config.x_axis_101, config.y_axis_101, config.z_axis_101);
+								}
 								if(config.sampling_interval_101_active){
 									promises.sampling_interval_101 = node.config_gateway.config_set_sampling_interval_101(mac, parseInt(config.sampling_interval_101));
 								}
